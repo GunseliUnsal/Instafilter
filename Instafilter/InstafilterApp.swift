64ef2b51fea6a0b48b4d9239097eadd6450d5b14
Gunseli Unsal <gunseliunsal@gmail.com>
2024-08-11 21:36:53 +0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Günseli Ünsal on 11.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
