3883aa2eaecda3f4979ca2dddd8b27a4deed0510
Gunseli Unsal <gunseliunsal@gmail.com>
2024-08-15 22:18:06 +0300

    build basic ui

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,17 +5,47 @@
 //  Created by Günseli Ünsal on 11.08.2024.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+
+                Spacer()
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+
+                    Spacer()
+
+                    // share the picture
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-        .padding()
+    }
+    func changeFilter() {
     }
 }
 
